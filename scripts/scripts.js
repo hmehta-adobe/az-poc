711903c0ea3b1d021cf66a82ae301fb547aed867
suresh <sryjgnnthds@gmail.com>
2026-05-29 19:27:42 +0530

    Comment out buildHeroBlock function call in scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -100,7 +100,7 @@ function buildAutoBlocks(main) {
       });
     }
 
-    buildHeroBlock(main);
+    // buildHeroBlock(main);
   } catch (error) {
     // eslint-disable-next-line no-console
     console.error('Auto Blocking failed', error);
